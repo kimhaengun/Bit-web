614a765160337f845239d26bd3b0b1feab18e9c8
kimhaengun <chxl89@naver.com>
2023-03-20 16:41:15 +0900

    HTML5규격의 websocket

--- boot07/src/main/java/com/bit/boot07/config/EchoHandler.java
+++ boot07/src/main/java/com/bit/boot07/config/EchoHandler.java
@@ -1,36 +1,47 @@
 package com.bit.boot07.config;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+
 public class EchoHandler extends TextWebSocketHandler {
 
-	static List<WebSocketSession> list = new ArrayList<>();
-	
+	private static Map<String,WebSocketSession> list = new HashMap<>();
+	public static Map<String, WebSocketSession> getList() {
+		return list;
+	}
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("접속 끊어짐~~~");
-		list.remove(session);
+		list.remove(session.getAttributes().get("sid"));
 	}
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		// TODO Auto-generated method stub
+//		session.sendMessage(new TextMessage("+"+session.getId()));
 		System.out.println("접속됨 Connect~~~");
-		list.add(session);
+		System.out.println("웹 소켓 : "+session.getId());
+//		for(Entry<String , Object> entry :  session.getAttributes().entrySet()) {
+//			System.out.println("세션 목록 : "+entry.getKey()+", "+entry.getValue());
+//		}
+		list.put((String)session.getAttributes().get("sid"),session);
 	}
 	
 	@Override
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("메세지 수신~~~"+message);
-		for(WebSocketSession sock : list) {
+		for(WebSocketSession sock : list.values()) {
 			sock.sendMessage(message);			
 		}
 	}
--- boot07/src/main/java/com/bit/boot07/config/SocketConfig.java
+++ boot07/src/main/java/com/bit/boot07/config/SocketConfig.java
@@ -1,18 +1,29 @@
 package com.bit.boot07.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
 public class SocketConfig implements WebSocketConfigurer{
 
+	@Bean
+	public WebSocketHandler getTextWebSocketHandler() {
+		return new EchoHandler();
+	}
+	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		WebSocketHandlerRegistration reg =  registry.addHandler(new EchoHandler(), "/echo");
+		WebSocketHandlerRegistration reg =  null;
+		reg =registry.addHandler(getTextWebSocketHandler(), "/echo");
+		reg.addInterceptors(new HttpSessionHandshakeInterceptor());
 		
 	}
 
